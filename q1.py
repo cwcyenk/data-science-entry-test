dc453370f6a326752e3f3f26e1a1d5e59e968703
Yen Kai <99772532+cwcyenk@users.noreply.github.com>
2025-03-19 12:10:24 +0800

    Uploaded Solution for Q1

---
+++ q1.py
@@ -0,0 +1,23 @@
+""" Solution for Question 1 """
+
+def swap(x, y):
+    """
+    - Swap the values of x and y if they are numeric.
+    - Else return -1 if either or both are not numeric. 
+    """
+    if isinstance(x, (int, float)) and isinstance(y, (int, float)) == True:
+        print("The original values of x and y are {} and {}, respectively.".format(x, y))
+        x, y = y, x
+        print("After swapping, the values of x and y becomes {} and {}, respectively.".format(x, y))
+    else:
+        return -1
+        
+""" Test scenario 1"""
+print("Test Scenario 1")
+swap("Apple", 10)
+
+print("\n")
+
+""" Test scenario 2 """
+print("Test Scenario 2")
+swap(9, 17)
